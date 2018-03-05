c44138040d3975839932b1de4925a1e1f4180626
Israel <213withani@gmail.com>
2018-03-04 20:46:12 -0800

    using braces for expressions

--- todoReactJS/main.js
+++ todoReactJS/main.js
@@ -1,15 +1,14 @@
 class Item extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-          list : ["Yo", "there", "hello"]
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+        list : ["Yo", "there", "hello"]
     }
-  
-    render() {
-      return <div>{this.state.list.map((item)=>console.log(item))}</div>;
-    }
-  } 
-  
-  ReactDOM.render(<Item />, document.getElementById("root"));
-  
+  }
+
+  render() {
+    return <div>{this.state.list.map((item)=><li>{item}</li>)}</div>;
+  }
+} 
+
+ReactDOM.render(<Item />, document.getElementById("root"));
