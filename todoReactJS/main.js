c4a3935015bf20bfcdb4786e2af3e889b46c1eb7
Israel <213withani@gmail.com>
2018-03-06 21:18:06 -0800

    simple todo works

--- todoReactJS/main.js
+++ todoReactJS/main.js
@@ -3,20 +3,36 @@ class Item extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      itemValue: ""
+      itemValue: "",
+      list: []
     };
     this.eventHandler = this.eventHandler.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this); 
+    
   }
 
   eventHandler(e) {
     this.setState({ itemValue: e.target.value });
   }
+  
+  handleSubmit(e) {
+    event.preventDefault()
+    this.setState({
+      itemValue: "",
+      list: [...this.state.list, this.state.itemValue]
+    });
+  }
 
   render() {
     return (
       <div>
-        <input type="text" onChange={this.eventHandler} value={this.state.itemValue}/>
-      <li>{this.state.itemValue}</li>
+        <form onSubmit={this.handleSubmit}>
+          <label>Todo</label>
+          <input type="text" onChange={this.eventHandler} value={this.state.itemValue}/>
+         </form>
+        
+         
+        <ul>{this.state.list.map((item)=><li>{item}</li>)} </ul>
       </div>
     );
   }
